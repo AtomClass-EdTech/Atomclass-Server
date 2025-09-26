c7c588ee4518751170c67d21f2530792554036c8
atom-nandeeshdb <markbotoffical@gmail.com>
2025-09-26 12:23:57 +0530

    hot-fix

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -27,26 +27,7 @@ router.post("/refresh-tokens", async (req, res, next) => {
     next(err);
   }
 });
-router.post("/refreshTokens", async (req, res, next) => {
-  try {
-    const { refreshToken, userIdentifier } = req.body;
-    if (!refreshToken || !userIdentifier) {
-      return res
-        .status(400)
-        .json({ error: "userIdentifier and refreshToken are required" });
-    }
-    const tokens = await authService.refreshTokens(
-      userIdentifier,
-      refreshToken,
-    );
-    return res.json({ payload: tokens });
-  } catch (err) {
-    console.error(err);
-    next(err);
-  }
-});
 router.post("/reset-password", authController.resetPassword);
-router.post("/resetPassword", authController.resetPassword);
 router.post("/forgot-password", authController.forgotPassword);
 router.post("/forgotPassword", authController.forgotPassword);
 
